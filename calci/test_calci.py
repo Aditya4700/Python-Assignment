583a6aea923ef0606cff53df4b6d88079f4c8c34
Aditya4700 <adityakolekar047@gmail.com>
2023-03-01 11:43:18 +0530

    updated

--- calci/test_calci.py
+++ calci/test_calci.py
@@ -1,46 +1,44 @@
+    
 import pytest
-from fun import add,subtract,multiply,divide
-def test_add():
-    value = add(10, 2)
-    assert value == 12
-    
-def test_addi():
-    value = add(-10, -2)
-    assert value == -12
+from fun import add, subtract, multiply, divide
 
+def test_addition_positive():
+    result = add(10, 2)
+    assert result == 12
 
-def test_subtract():
-    value = subtract(12, 11)
-    assert value == 1
-    
-def test_subtracti():
-    value = subtract(-2, -11)
-    assert value == 9
+def test_addition_negative():
+    result = add(-10, -2)
+    assert result == -12
 
+def test_subtraction_positive():
+    result = subtract(12, 11)
+    assert result == 1
 
-def test_subtract_negative():
-    value = subtract(10, 11)
-    assert value == -1.0
+def test_subtraction_negative():
+    result = subtract(-2, -11)
+    assert result == 9
 
+def test_subtraction_negative_result():
+    result = subtract(10, 11)
+    assert result == -1.0
 
-def test_multiply():
-    value = multiply(6, 1)
-    assert value == 6.0
+def test_multiplication():
+    result = multiply(6, 1)
+    assert result == 6.0
 
-def test_multipli():
-    value = multiply(6, 0)
-    assert value == 0
+def test_multiplication_by_zero():
+    result = multiply(6, 0)
+    assert result == 0
 
+def test_division_positive():
+    result = divide(3, 2)
+    assert result == 1.5
 
-def test_divide():
-    value = divide(3, 2)
-    assert value == 1.5
+def test_division_negative():
+    result = divide(4, -2)
+    assert result == -2
 
-def test_divisi():
-    value = divide(4, -2)
-    assert value == -2
-    
-def test_divide_by_zero():
+def test_division_by_zero():
     with pytest.raises(ZeroDivisionError) as e:
-        value=divide(7,0)
-    assert "division by zero" in str(e.value)    
+        result = divide(7,0)
+    assert "division by zero" in str(e.value)
